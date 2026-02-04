22d5fee0fbea31d9aa18c48980665024d7401540
Satindra Ramalingam <satindra.r@gmail.com>
2026-02-04 19:23:21 +0530

    add user directory creation and archiving

--- src/services/user_service.rs
+++ src/services/user_service.rs
@@ -5,6 +5,7 @@ use std::fs::OpenOptions;
 use std::io;
 use std::io::Result;
 use std::io::Write;
+use std::path::Path;
 use std::process::Command;
 
 pub fn user_exists(username: &str) -> io::Result<bool> {
@@ -23,15 +24,9 @@ pub fn group_exists(group: &str) -> bool {
 }
 
 pub fn create_user(user: &str) -> io::Result<()> {
-    let home_dir = format!("/opt/watchdog/users/{}", user);
-
     let output = Command::new("sudo")
         .arg("useradd")
-        .arg("-m")
-        .arg("-d")
-        .arg(&home_dir)
-        .arg("--skel")
-        .arg("/etc/skel")
+        .arg("-M")
         .arg(user)
         .output()?;
 
@@ -44,15 +39,29 @@ pub fn create_user(user: &str) -> io::Result<()> {
         return Err(io::Error::other("Failed to create user"));
     }
 
-    match update_user_bashrc(user) {
-        Ok(_) => {
-            info!(target:get_log_target(),"User {} bashrc updated successfully.", user);
+    match create_user_directory(user) {
+        Ok(msg) => {
+            info!(target:get_log_target(),"User {} directory {} successfully.", user, msg);
+            if msg == "created" {
+                match update_user_bashrc(user) {
+                    Ok(_) => {
+                        info!(target:get_log_target(),"User {} bashrc updated successfully.", user);
+                    }
+                    Err(e) => {
+                        error!(target:get_log_target(), "Failed to update user {} bashrc: {}", user, e);
+                    }
+                }
+            }
         }
         Err(e) => {
-            error!(target:get_log_target(), "Failed to update user {} bashrc: {}", user, e);
+            error!(target:get_log_target(),
+                "Failed to create/archive user directory'{}': {}",
+                user,
+                e
+            );
+            return Err(io::Error::other("Failed to create/archive user directory"));
         }
     }
-
     Ok(())
 }
 
@@ -128,27 +137,107 @@ pub fn remove_user_from_group(user: &str, group: &str) -> io::Result<()> {
 }
 
 pub fn delete_user(user: &str) -> io::Result<()> {
-    let output = Command::new("sudo")
-        .arg("userdel")
-        .arg("-r")
-        .arg(user)
-        .output()?;
+    let output = Command::new("sudo").arg("userdel").arg(user).output()?;
 
     if output.status.success() {
         info!(target:get_log_target(), "User '{}' deleted successfully.", user);
-        Ok(())
     } else {
         error!(target:get_log_target(),
             "Failed to delete user '{}': {}",
             user,
             String::from_utf8_lossy(&output.stderr)
         );
-        Err(io::Error::other("Failed to delete user"))
+        return Err(io::Error::other("Failed to delete user"));
+    }
+
+    match archive_user_directory(user) {
+        Ok(_) => {
+            info!(target:get_log_target(), "User '{}' directory archived successfully.", user);
+            Ok(())
+        }
+        Err(e) => {
+            error!(target:get_log_target(),
+            "Failed to delete user '{}': {}",
+            user,e);
+            Err(io::Error::other("Failed to archive user directory"))
+        }
     }
 }
 
+pub fn create_user_directory(user: &str) -> Result<&'static str> {
+    let home_path = format!("/opt/watchdog/users/{}", user);
+    if Path::new(&home_path).exists() {
+        let output = Command::new("sudo")
+            .arg("chown")
+            .arg("-R")
+            .arg(format!("{}:{}", user, user))
+            .arg(&home_path)
+            .output()?;
+        if !output.status.success() {
+            return Err(io::Error::other(String::from_utf8_lossy(&output.stderr)));
+        }
+        Ok("unarchived")
+    } else {
+        fs::create_dir_all(&home_path)?;
+        let output1 = Command::new("sudo")
+            .arg("cp")
+            .arg("-r")
+            .arg("/etc/skel/.") // The /. ensures hidden files are copied
+            .arg(&home_path)
+            .output()?;
+        if !output1.status.success() {
+            return Err(io::Error::other(String::from_utf8_lossy(&output1.stderr)));
+        }
+
+        let output2 = Command::new("sudo")
+            .arg("chown")
+            .arg("-R")
+            .arg(format!("{}:{}", user, user))
+            .arg(&home_path)
+            .output()?;
+        if !output2.status.success() {
+            return Err(io::Error::other(String::from_utf8_lossy(&output2.stderr)));
+        }
+
+        let output3 = Command::new("sudo")
+            .arg("chmod")
+            .arg("-R")
+            .arg("700")
+            .arg(&home_path)
+            .output()?;
+        if !output3.status.success() {
+            return Err(io::Error::other(String::from_utf8_lossy(&output3.stderr)));
+        }
+
+        let output4 = Command::new("sudo")
+            .arg("usermod")
+            .arg("-d")
+            .arg(&home_path)
+            .arg(user)
+            .output()?;
+        if !output4.status.success() {
+            return Err(io::Error::other(String::from_utf8_lossy(&output4.stderr)));
+        }
+        Ok("created")
+    }
+}
+
+pub fn archive_user_directory(user: &str) -> Result<()> {
+    let home_path = format!("/opt/watchdog/users/{}", user);
+    let output = Command::new("sudo")
+        .arg("chown")
+        .arg("-R")
+        .arg("root:root")
+        .arg(&home_path)
+        .output()?;
+    if !output.status.success() {
+        return Err(io::Error::other(String::from_utf8_lossy(&output.stderr)));
+    }
+    Ok(())
+}
+
 pub fn update_user_bashrc(user: &str) -> Result<()> {
-    let bashrc_path = format!("opt/watchdog/users/{}/.bashrc", user);
+    let bashrc_path = format!("/opt/watchdog/users/{}/.bashrc", user);
     let bashrc_lines = r#"
 # Load group-specific config if present
 for group in $(id -nG "$USER"); do
